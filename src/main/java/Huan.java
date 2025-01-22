840c49786ec476aea89d20d4b0840feef55953b2
Huan-Kiat <limhk8902@gmail.com>
2025-01-22 23:08:06 +0800

    Implement Level-0

---
+++ src/main/java/Huan.java
@@ -0,0 +1,14 @@
+public class Huan {
+    private static String line = "____________________________________________________________";
+    
+    public static void main(String[] args) {
+        String greeting =  "Hello! I'm HUAN \n"
+                + "What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(line);
+        System.out.println(greeting);
+        System.out.println(line);
+        System.out.println(exit);
+        System.out.println(line);
+    }
+}
