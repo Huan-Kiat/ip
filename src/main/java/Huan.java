582575780c588c035a9c06903a5b9041cfe369d5
Huan-Kiat <limhk8902@gmail.com>
2025-01-22 23:09:43 +0800

    Implement Level-1

--- src/main/java/Huan.java
+++ src/main/java/Huan.java
@@ -1,14 +1,30 @@
+import java.util.Scanner;
+
 public class Huan {
     private static String line = "____________________________________________________________";
-    
+    private static String space = "    ";
     public static void main(String[] args) {
-        String greeting =  "Hello! I'm HUAN \n"
-                + "What can I do for you?";
-        String exit = "Bye. Hope to see you again soon!";
-        System.out.println(line);
+        Scanner scanner = new Scanner(System.in);
+        String greeting =  space + "Hello! I'm HUAN \n"
+                + space + "What can I do for you?";
+        String exit = space + "Bye. Hope to see you again soon!";
+        System.out.println(space + line);
         System.out.println(greeting);
-        System.out.println(line);
-        System.out.println(exit);
-        System.out.println(line);
+        System.out.println(space + line);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println(space + line);
+                System.out.println(exit);
+                System.out.println(space + line);
+                break;
+            }
+            System.out.println(space + line);
+            System.out.println(space + input);
+            System.out.println(space + line);
+        }
+
+        scanner.close();
     }
 }
